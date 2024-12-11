e9b48f156d95e84e859b9701ebb974870f424800
alireza flh <alirezaflh70@gmail.com>
2024-12-11 10:04:39 +0330

    Add BaseAdminController and FeedbackFormsController
    Introduce BaseAdminController with role-based authorization and
    message properties. Add FeedbackFormsController with route
    definition, authentication, and feedback form creation methods.

---
+++ Puzzle/Controllers/BaseAdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Puzzle.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class BaseAdminController : Controller
+    {
+        public static string SuccessMessage = "SuccessMessage";
+        public static string ErrorMessage = "ErrorMessage";
+        public static string InfoMessage = "InfoMessage";
+        public static string WarningMessage = "WarningMessage";
+        public static string ErrorMessageWithURL = "ErrorMessageWithURL";
+    }
+}
---
+++ Puzzle/Controllers/FeedbackFormsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Puzzle.Services.Interfaces;
+using Puzzle.ViewModels.Feedbacks;
+using System.Threading.Tasks;
+
+namespace Puzzle.Controllers
+{
+    [Route("[controller]/")]
+    [Authorize]
+    public class FeedbackFormsController : Controller
+    {
+        #region Fields
+
+        private readonly IFeedbackFormService _feedbackFormService;
+
+        #endregion
+
+        #region Ctor
+
+        public FeedbackFormsController(IFeedbackFormService feedbackFormService)
+        {
+            _feedbackFormService = feedbackFormService;
+        }
+
+        #endregion
+
+        [HttpGet]
+        public IActionResult Create(string designerId)
+        {
+            if (string.IsNullOrEmpty(designerId))
+            {
+                return BadRequest();
+            }
+
+            var model = new UpsertFeedbackFormViewModel
+            {
+                DesignerId = designerId
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UpsertFeedbackFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await _feedbackFormService.CreateAsync(model);
+
+            if (result.IsFailure)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Index", "Home",);
+        }
+
+    }
+}
